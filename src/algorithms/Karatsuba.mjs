b3b13b7f4ef6d00cf57005fdb1b9f8596f55a728
lebedevhh <lebedevhh@outlook.fr>
2023-01-12 11:45:22 -0500

    Made the karatsuba function generalized

--- src/algorithms/Karatsuba.mjs
+++ src/algorithms/Karatsuba.mjs
@@ -1,6 +1,5 @@
 //in the name of the most merciful the all merciful
 
-//required for integers only
 //source code : https://gist.github.com/syphh/0df7faf18a0412346dacefe8a213da4b
 export default function karatsuba(x, y){
     if (x < 10 && y < 10) return x * y;
@@ -18,4 +17,43 @@ export default function karatsuba(x, y){
         let result = ac * (10 ** (2 * half)) + (ad_plus_bc * (10 ** half)) + bd;
         return result;
     }
-}
+};
+
+//is there a way of doing this with decimal numbers
+//karatsuba generalized (works with decimals, we need)
+function getScientificNotation(number){
+    if (Number.isInteger(number)){
+        return karatsuba(number)
+    }
+    
+    else{
+        let n = 0;
+        while (!Number.isInteger(number)){
+            number *= 10;
+            n--;
+        }
+        let remaining = number;
+        //returning a list with the number and the exponent
+        let list = [remaining, n];
+        // console.log(`${remaining} * 10 ** ${n}`);
+        return list;
+    }
+
+};
+
+// console.log(getScientificNotation(1.6774));
+function karatsubaGeneralized(x, y){
+    x = getScientificNotation(x);
+    y = getScientificNotation(y);
+    // console.log(x);
+    // console.log(y);
+    let product = karatsuba(x[0], y[0]);
+    // console.log(product);
+    let exponent = x[1] + y[1];
+    // console.log()
+    let result = product * (10 ** exponent);
+    return result;
+};
+
+
+console.log(karatsubaGeneralized(1.5, 4.5));
